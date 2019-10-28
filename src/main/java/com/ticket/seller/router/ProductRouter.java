a43f5451fa8114698d55bd0fac9c8cb32027a14c
rbrbrb7290 <ohg429@gmail.com>
2019-10-28 11:22:46 +0900

    S3 async config

--- src/main/java/com/ticket/seller/router/ProductRouter.java
+++ src/main/java/com/ticket/seller/router/ProductRouter.java
@@ -6,14 +6,9 @@ import com.ticket.seller.handler.TicketHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import reactor.core.publisher.Mono;
-
-import java.util.Map;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
@@ -29,23 +24,12 @@ public class ProductRouter {
                 .nest(path("/"),
                         route(GET("/"), healthHandler::checkHealth))
                 .andNest(path("/api/product"),
-                        route(GET("/")
-                            .and(accept(MediaType.APPLICATION_JSON_UTF8)),productHandler::findAll)
-                        .andRoute(GET("/saveTest")
-                            .and(accept(MediaType.APPLICATION_JSON_UTF8)), productHandler::saveTest)
-                        .andRoute(GET("/save")
-                            .and(accept(MediaType.APPLICATION_JSON_UTF8)), productHandler::save)
-                        .andRoute(GET("/{seller}")
-                            .and(accept(MediaType.APPLICATION_JSON_UTF8)), productHandler::findSeller)
-                )
+                        route(GET("/"),productHandler::findAll)
+                            .andRoute(POST("/save"), productHandler::save)
+                            .andRoute(GET("/{seller}") , productHandler::findSeller))
                 .andNest(path("/api/ticket"),
-                        route(GET("/")
-                            .and(accept(MediaType.APPLICATION_JSON_UTF8)),ticketHandler::findAll)
-                        .andRoute(GET("/{userId}")
-                            .and(accept(MediaType.APPLICATION_JSON_UTF8)), ticketHandler::findUserByTicket)
-//                        .andRoute(GET("/save")
-//                            .and(contentType(MediaType.APPLICATION_JSON_UTF8)), ticketHandler::save)
-                );
-
+                        route(GET("/"),ticketHandler::findAll)
+                            .andRoute(GET("/{userId}"), ticketHandler::findUserByTicket));
+//                          .andRoute(GET("/save"), ticketHandler::save));
     }
 }
--- src/test/java/com/ticket/seller/repository/ProductRepositoryTests.java
+++ src/test/java/com/ticket/seller/repository/ProductRepositoryTests.java
@@ -1,47 +1,60 @@
 package com.ticket.seller.repository;
 
-import com.ticket.seller.config.dynamoDbConfig;
-import org.junit.Ignore;
+import com.ticket.seller.objectMapper.ProductMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import software.amazon.awssdk.services.dynamodb.model.ListTablesRequest;
-import software.amazon.awssdk.services.dynamodb.model.ListTablesResponse;
+import reactor.core.publisher.Mono;
+import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
+import software.amazon.awssdk.services.dynamodb.model.PutItemResponse;
 
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
+import java.util.Map;
+import java.util.UUID;
 
 @RunWith(SpringRunner.class)
 @AutoConfigureWebTestClient
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ProductRepositoryTests {
     @Autowired
-    private dynamoDbConfig client;
+    private DynamoDbAsyncClient client;
+    @Autowired
+    private ProductMapper productMapper;
+//    @Autowired
+//    private AmazonS3Config amazonS3Config;
 
     @Test
-    @Ignore
-    public void listTable(){
-        ListTablesRequest request = ListTablesRequest.builder().build();
-        CompletableFuture<ListTablesResponse> response
-                = client.dynamoDbAsyncClient().listTables(request);
-        CompletableFuture<List<String>> tableNames = response.thenApply(ListTablesResponse::tableNames);
-        tableNames.whenComplete((tables, err) -> {
-            try {
-                if (tables != null) {
-                    tables.forEach(System.out::println);
-                    System.out.println("Table List : " + tables);
-                } else {
-                    // Handle error
-                    err.printStackTrace();
-                }
-            } finally {
-                // Lets the application shut down. Only close the client when you are completely done with it.
-                client.dynamoDbAsyncClient().close();
-            }
-        });
-        tableNames.join();
+//    @Ignore
+    public void createProduct() {
+        PutItemRequest putItemRequest = PutItemRequest.builder()
+                .tableName("ticket")
+                .item(
+                        Map.ofEntries(
+                            Map.entry("PK", AttributeValue.builder().s("Test").build()),
+                            Map.entry("SK", AttributeValue.builder().s(UUID.randomUUID().toString()).build()),
+                            Map.entry("date", AttributeValue.builder().s("ZZZZZ").build()),
+                            Map.entry("name", AttributeValue.builder().s("createTest").build()),
+                            Map.entry("seller_id", AttributeValue.builder().s("Tester").build()),
+                            Map.entry("image" , AttributeValue.builder().s("url").build()),
+                            Map.entry("info" , AttributeValue.builder().s("테스트임당").build()),
+                            Map.entry("option" , AttributeValue.builder().s("더미").build()),
+                            Map.entry("area" , AttributeValue.builder().s("SEOUL").build()),
+                            Map.entry("price" , AttributeValue.builder().n(String.valueOf(7777)).build()),
+                            Map.entry("category" , AttributeValue.builder().s("TOUR").build()),
+                            Map.entry("sub_category" , AttributeValue.builder().s("ATTRACTION").build())
+                        ))
+                .build();
+
+             Mono.fromFuture(client
+                .putItem(putItemRequest)
+                .thenApplyAsync(PutItemResponse::attributes)
+                .thenApplyAsync(productMapper::toObj));
+//             client.getItem(GetItemRequest.builder().build())
     }
+
+
 }
